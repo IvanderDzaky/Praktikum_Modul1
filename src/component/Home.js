5c07b150f4af7b368bfce7062c550d9209ff5218
IvanderDzaky <75924357+IvanderDzaky@users.noreply.github.com>
2022-05-16 20:08:34 +0800

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+            <p className='location-header'>Practicum 1/Home</p>
+            <br></br><br></br><br></br><br></br><br /><br /><br />
+            <p className="card-text lead fs-2 mb-0">THIS IS PRAKTIKUM 1</p>
+            <p className="card-text lead mb-5">By Vann</p>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+        </div>
+
+    );
+};
+
+export default Home;
